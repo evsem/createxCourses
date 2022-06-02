7dd63357ea630debe3fb5281f0a2238fcbbf57d6
evsem <frukwork@gmail.com>
2022-06-02 19:42:52 +0300

    ver.3

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -85,3 +85,16 @@ function openMenuMobile(e) {
 function renderMenuMobile() {
   mobile.appendChild(menu)
 }
+
+// ANCHORS
+let anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault()
+    const blockID = anchor.getAttribute("href")
+    document.querySelector("" + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
+  })
+}
